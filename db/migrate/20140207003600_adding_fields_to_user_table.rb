b9cb73a57b9a7fe69b7d89a763f3f1bbfc5645d5
Katya Sarmiento <kittyserenakat@gmail.com>
2014-02-13 23:41:36 -0500

    fixing migration email dup error

--- db/migrate/20140207003600_adding_fields_to_user_table.rb
+++ db/migrate/20140207003600_adding_fields_to_user_table.rb
@@ -2,7 +2,7 @@ class AddingFieldsToUserTable < ActiveRecord::Migration
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :email, :string
+    # add_column :users, :email, :string
     add_column :users, :bio, :text
   end
 end
