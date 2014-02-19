998a7bb02c0cba268d08e4d59d26bf1cbf2aa1ae
Diego Salazar <salazar@kipusystems.com>
2014-02-19 13:45:17 -0500

    added bugsnag

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ActiveRecord::Base
   scope :all_not_me, where("email != ?", "kittyserenakat@gmail.com")
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "120x120>" }, default_url: "http://placekitten.com/120/120"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   def name 
     "#{first_name} #{last_name}"
